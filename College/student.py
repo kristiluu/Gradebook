2378d508723fde44680614a9ba4215495ced652b
Kristi Luu <42658784+kristiluu@users.noreply.github.com>
2019-09-11 23:25:39 -0700

    Update and rename student.py to College/student.py

--- College/student.py
+++ College/student.py
@@ -1,4 +1,4 @@
-#student.py module for lab4.py
+#student.py module for student record program
 GRADEBOOK = { 'A+': 4.0, 'A': 4.0, 'A-': 3.7,     
               'B+': 3.3, 'B': 3.0, 'B-': 2.7,    
               'C+': 2.3, 'C': 2.0, 'C-': 1.7,     
